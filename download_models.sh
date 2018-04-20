c343ea855069740b8c2ed5cf8215532b8703ff4d
Anne Schumacher <anne.schumacher@gu.se>
2018-04-20 16:05:31 +0200

    bug fix

--- download_models.sh
+++ download_models.sh
@@ -3,17 +3,16 @@
 echo Downloading FreeLing models
 mkdir freeling
 v=4.0
-cd freeling
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/ca.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/de.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/en.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/es.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/fr.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/gl.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/it.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/no.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/pt.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/ru.cfg
-wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/sl.cfg
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/ca.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/de.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/en.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/es.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/fr.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/gl.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/it.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/no.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/pt.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/ru.cfg -P freeling
+wget -q https://github.com/TALP-UPC/FreeLing/raw/$v/data/config/sl.cfg -P freeling
 
 echo Done.
